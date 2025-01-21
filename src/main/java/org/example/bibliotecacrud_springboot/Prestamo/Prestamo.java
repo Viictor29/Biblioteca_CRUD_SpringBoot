fd3eacd8d5b0bf53550b4c35036b27fd4546e8de
Victor <almenamvictor@gmail.com>
2025-01-21 21:29:22 +0100

    Ultimo commit

--- src/main/java/org/example/bibliotecacrud_springboot/Prestamo/Prestamo.java
+++ src/main/java/org/example/bibliotecacrud_springboot/Prestamo/Prestamo.java
@@ -1,9 +1,8 @@
 package org.example.bibliotecacrud_springboot.Prestamo;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.example.bibliotecacrud_springboot.Ejemplar.Ejemplar;
+import org.example.bibliotecacrud_springboot.Usuario.Usuario;
 
 import java.time.LocalDate;
 
@@ -12,8 +11,55 @@ public class Prestamo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private int usuario_id;
-    private int ejemplar_id;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id") // Relación con la tabla Usuario
+    private Usuario usuario;
+
+    @ManyToOne
+    @JoinColumn(name = "ejemplar_id") // Relación con la tabla Ejemplar
+    private Ejemplar ejemplar;
+
     private LocalDate fechaInicio;
     private LocalDate fechaDevolucion;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Ejemplar getEjemplar() {
+        return ejemplar;
+    }
+
+    public void setEjemplar(Ejemplar ejemplar) {
+        this.ejemplar = ejemplar;
+    }
+
+    public LocalDate getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(LocalDate fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public LocalDate getFechaDevolucion() {
+        return fechaDevolucion;
+    }
+
+    public void setFechaDevolucion(LocalDate fechaDevolucion) {
+        this.fechaDevolucion = fechaDevolucion;
+    }
 }
